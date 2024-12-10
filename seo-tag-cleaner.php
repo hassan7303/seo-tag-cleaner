0d663a29a709820f4fed9d3410022980325d7a85
hsnali <hassanali7303@gmail.com>
2024-12-10 11:56:47 +0330

    added updater code

--- seo-tag-cleaner.php
+++ seo-tag-cleaner.php
@@ -100,3 +100,66 @@
      // Replace element
      $element->parentNode->replaceChild($newDiv, $element);
  }
+
+
+
+
+  /**
+ * Checks for plugin updates from GitHub and notifies WordPress.
+ *
+ * Fetches the latest release information from GitHub. If a newer version is available,
+ * it adds the update to the WordPress update queue.
+ *
+ * @param object $transient The transient object containing update information.
+ * @return object Modified transient object with update details if available.
+ */
+function check_for_plugin_update($transient) {
+    if (empty($transient->checked)) {
+        return $transient;
+    }
+  
+    $plugin_slug = plugin_basename(__FILE__);
+    $github_url = 'https://api.github.com/repos/hassan7303/seo-tag-cleaner/releases/latest';
+  
+    $response = wp_remote_get($github_url, ['sslverify' => false]);
+    if (is_wp_error($response)) {
+        return $transient;
+    }
+  
+    $release_info = json_decode(wp_remote_retrieve_body($response), true);
+  
+    if (isset($release_info['tag_name']) && isset($transient->checked[$plugin_slug])) {
+        $new_version = $release_info['tag_name'];
+        if (version_compare($transient->checked[$plugin_slug], $new_version, '<')) {
+            $transient->response[$plugin_slug] = (object) [
+                'slug' => $plugin_slug,
+                'new_version' => $new_version,
+                'package' => $release_info['zipball_url'],
+                'url' => 'https://github.com/hassan7303/seo-tag-cleaner',
+            ];
+        }
+    }
+  
+    return $transient;
+  }
+  add_filter('pre_set_site_transient_update_plugins', 'check_for_plugin_update');
+  
+  /**
+  * Adjust plugin folder name after installation to maintain original name.
+  */
+  function fix_plugin_folder_name($response, $hook_extra, $result) {
+    global $wp_filesystem;
+  
+    $plugin_slug = 'seo-tag-cleaner';
+    $original_folder = WP_PLUGIN_DIR . '/' . $plugin_slug;
+    $new_folder = $result['destination'];
+  
+    if (basename($new_folder) !== $plugin_slug) {
+        $wp_filesystem->move($new_folder, $original_folder);
+        $result['destination'] = $original_folder;
+    }
+  
+    return $response;
+  }
+  add_filter('upgrader_post_install', 'fix_plugin_folder_name', 10, 3);
+  
