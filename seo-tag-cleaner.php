fcf1be89ac08b49f2c1ccab242e173caa3b3bb84
hsnali <hassanali7303@gmail.com>
2024-12-10 11:49:29 +0330

    init

---
+++ seo-tag-cleaner.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Plugin Name: SEO Tag Cleaner
+ *
+ * Description: Optimizes H1, footer, and header tags for better SEO by managing duplicates.
+ *
+ * Version: 1.0.0
+ *
+ * Author: hassan Ali Askari
+ * Author URI: https://t.me/hassan7303
+ * Plugin URI: https://github.com/hassan7303
+ *
+ * License: MIT
+ * License URI: https://opensource.org/licenses/MIT
+ *
+ * Email: hassanali7303@gmail.com
+ * Domain Path: https://hsnali.ir
+ */
+
